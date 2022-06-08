b2a3d10c1cbe5169242e80fc2558b372a051227f
Антон <a.a.pavlenko89@mail.ru>
2022-06-08 22:16:39 +0300

    ДЗ 2.2

--- js/main.js
+++ js/main.js
@@ -1,17 +1,31 @@
 // Источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-const random = function getRandomIntInclusive(min, max) {
-  if (max <= min) {
-    console.log('Максимально допустимое число!')
+function getRandomIntInclusive(min, max) {
+  if (typeof min !== 'number' || typeof max !== 'number') {
+    return -1;
+  };
+
+  if (min < 0 || max < 0) {
+    return -1;
   }
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+
+  if (max <= min) {
+    return -1;
+  }
+
+  const minCeil = Math.ceil(min);
+  const maxFloor = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloor - minCeil + 1)) + minCeil;
 }
 
-random(0, 45);
-console.log('Случайное число равно ' + ': ' + random(0, 10));
+getRandomIntInclusive(0, 45);
 
-// Источник https://developer.mozilla.org/ru/docs/Learn/JavaScript/First_steps/Useful_string_methods#строки_как_объекты
-let browserType = 'mozilla';
-browserType = browserType.length;
-console.log('Количество символов равно ' + ': ' + browserType);
+
+function maxLengthString(checkString, maxLength) {
+  if (checkString.length >= maxLength) {
+    return true;
+  }
+
+    return false;
+}
+
+maxLengthString('ааааааааа', 45);
